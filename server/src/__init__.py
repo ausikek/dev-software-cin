74418d29347333b49fa015fba57739f615d88ad0
ausikek <orapacen2@outlook.com>
2024-12-01 21:06:08 -0300

    server, client and db up and running

--- server/src/__init__.py
+++ server/src/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+from .settings.config import Config
+
+mongo = PyMongo()
 
 app = Flask(__name__)
+
+app.config.from_object(Config)
+
+mongo.init_app(app)
 
 from .views import views
