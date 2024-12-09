bc2a28cdd870262f7064cfdd6e67833b51b07152
ausikek <orapacen2@outlook.com>
2024-12-09 09:34:06 -0300

    fix/ChatButton: now chatbutton is a div and a button, defined by props

--- client/src/components/ChatButton/index.tsx
+++ client/src/components/ChatButton/index.tsx
@@ -1,18 +1,34 @@
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import React from "react";
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import React from 'react';
 
-type ButtonProps = React.HTMLAttributes<HTMLButtonElement>;
+type ButtonProps = React.HTMLAttributes<HTMLButtonElement> & {
+  variant: 'bubble' | 'button';
+};
 
-export default function ChatButton({ ...props }: ButtonProps) {
-  return (
-    <Button
-      className={cn(
-        "w-[13em] h-[2.5em] rounded-3xl bg-[#464646] hover:bg-[#464646]/90 text-white",
-        props.className
-      )}
-    >
-      {props.children}
-    </Button>
-  );
+export default function ChatButton({ variant, ...props }: ButtonProps) {
+  if (variant == 'button') {
+    return (
+      <Button
+        {...props}
+        className={cn(
+          'w-[13rem] h-[2.5rem] rounded-3xl bg-[#464646] hover:bg-[#464646]/90 text-white',
+          props.className
+        )}
+      >
+        {props.children}
+      </Button>
+    );
+  } else {
+    return (
+      <div
+        className={cn(
+          'w-auto min-w-[13rem] max-w-[30rem] h-auto min-h-[2.5rem] rounded-3xl bg-[#464646] hover:bg-[#464646]/90 text-white flex items-center justify-center p-4',
+          props.className
+        )}
+      >
+        {props.children}
+      </div>
+    );
+  }
 }
